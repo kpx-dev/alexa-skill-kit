6ff7eb9f53091af67b8ccf9dca7365caa9aae9be
Kien Pham <shop@kienpham.com>
2017-08-03 23:10:20 -0700

    adding deploy

--- setup.py
+++ setup.py
@@ -1,8 +1,10 @@
 """
 Alexa Skill Kit for Python
--------------
+--------------------------
 
-Help you create Alexa skill super easy in Python
+Help you create Alexa skill super easy in Python, like 20 lines of code, promise!
+
+Please go to the repo for more info on getting started: https://github.com/KNNCreative/alexa-skill-kit
 """
 from setuptools import setup, find_packages
 from alexa_skill_kit import __version__
@@ -13,7 +15,7 @@ setup(
     url='https://github.com/KNNCreative/alexa-skill-kit',
     license='MIT',
     author='Kien Pham',
-    author_email='info@knncreative.com',
+    author_email='kien@knncreative.com',
     description='Alexa Skill Kit for Python',
     long_description=__doc__,
     packages=['alexa_skill_kit'],
@@ -25,6 +27,7 @@ setup(
         'requests',
     ],
     test_suite='tests',
+    keywords = ['alexa skill kit', 'alexa', 'skill kit', 'alex echo', 'alexa ask', 'aws alexa'],
     classifiers=[
         'License :: OSI Approved :: Apache Software License',
         "Programming Language :: Python",
